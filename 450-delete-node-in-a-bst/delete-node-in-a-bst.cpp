9f17177a462e07a863c2451f0ec25857c7474835
Sumantra Singh <singhsumantra920@gmail.com>
2026-02-14 12:24:06 +0530

    Time: 2 ms (2.14%) | Memory: 34.4 MB (6.50%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -12,22 +12,56 @@
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-         if (root)
-            if (key < root->val)
-                root->left = deleteNode(root->left, key);
-            else if (key > root->val)
-                root->right = deleteNode(root->right, key);
-            else {
-                if (!root->left && !root->right)
-                    return NULL;
-                if (!root->left || !root->right)
-                    return root->left ? root->left : root->right;
-                TreeNode* temp = root->left;
-                while (temp->right != NULL)
-                    temp = temp->right;
-                root->val = temp->val;
-                root->left = deleteNode(root->left, temp->val);
+        if(!root) return nullptr;
+        if(root->val<key){
+            root->right=deleteNode(root->right,key);
+        }
+        else if(root->val>key){
+            root->left=deleteNode(root->left,key);
+        }
+        else{
+            //leaf node
+            if(!root->left && !root->right){
+                delete root;
+                return nullptr;
             }
+            //right child exist
+
+            else if(!root->left){
+                TreeNode*temp=root->right;
+                delete root;
+                return temp;
+            }
+            //left child exist
+            else if(!root->right){
+                TreeNode*temp=root->left;
+                delete root;
+                return temp;
+            }
+            //both child exist 
+            else{
+                TreeNode*parent=root;
+                TreeNode*child=parent->left;
+                //find greatest elemnt in left side of parent
+                while(child->right){
+                    parent=child;
+                    child=child->right;
+                }
+                if(parent==root){
+                    //iss case ka matlab hai child k right me koi node hi nahi hai 
+                    child->right=parent->right;
+                    delete root;
+                    return child;
+                }
+                else{
+                    parent->right=child->left;
+                    child->left=root->left;
+                    child->right=root->right;
+                    delete root;
+                    return child;
+                }
+            }
+        }
         return root;
     }
 };
