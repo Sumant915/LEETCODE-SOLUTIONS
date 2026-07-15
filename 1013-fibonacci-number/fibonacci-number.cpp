f1d893f4d7e3b7c24ae8b9b8227f5ec3de9aa0fb
Sumantra Singh <singhsumantra920@gmail.com>
2026-07-15 11:39:33 +0530

    Time: 2 ms (78.43%) | Memory: 8 MB (20.71%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    void findfibo(int n,vector<int>&ans){
-        for(int i=2;i<=n;i++){
-            ans[i]=ans[i-1]+ans[i-2];
-        }
-    }
     int fib(int n) {
         if(n<=1) return n;
         if(n==2) return 1;
-        vector<int>ans(n+1,-1);
-        ans[0]=0;
-        ans[1]=1;
-        findfibo(n,ans);
-        return ans[n];
+        vector<int>dp(3);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+            dp[2]=dp[0]+dp[1];
+        }
+        return dp[2];
     }
 };
