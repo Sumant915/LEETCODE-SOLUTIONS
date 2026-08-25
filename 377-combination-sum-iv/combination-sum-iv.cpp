327b830c258329ce2ae0a0f59355e7b2dd8dc981
Sumantra Singh <singhsumantra920@gmail.com>
2026-08-26 00:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (15.93%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-   int totalcombination(int target,vector<int>&nums,vector<int>&dp){
-    if(target==0) return 1;
-    if(target<0) return 0;
-    if(dp[target]!=-1) return dp[target];
-    int total=0;
-    for(int i=0;i<nums.size();i++){
-        total+=totalcombination(target-nums[i],nums,dp);
-    }
-    return dp[target]=total;
-   }
     int combinationSum4(vector<int>& nums, int target) {
         vector<long long>dp(target+1,0);
         dp[0]=1;
