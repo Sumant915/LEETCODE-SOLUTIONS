8b5f545867ab9ba0a85783f520095b46d7923d81
Sumantra Singh <singhsumantra920@gmail.com>
2026-09-01 23:59:19 +0530

    Time: 468 ms (14.81%) | Memory: 225.5 MB (13.20%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findans(int index,int n,int buy,int transaction,vector<int>&prices, vector<vector<vector<int>>>&dp){
+        if(index==n || transaction==0) return 0;
+        if(dp[index][buy][transaction]!=-1) return dp[index][buy][transaction];
+        if(buy==1){
+            return dp[index][buy][transaction]=max(-prices[index]+findans(index+1,n,0,transaction,prices,dp),
+            findans(index+1,n,1,transaction,prices,dp));
+        }
+        else{
+            return dp[index][buy][transaction]= max(prices[index]+findans(index+1,n,1,transaction-1,prices,dp),
+            findans(index+1,n,0,transaction,prices,dp));
+        }
+
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+        return findans(0,n,1,2,prices,dp);
+    }
+};
